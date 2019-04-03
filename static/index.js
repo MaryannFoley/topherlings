14a7fd1260069e491bea8061096b9406527889b6
JoanChirinos <jchirinos@stuy.edu>
2019-04-03 13:36:07 -0400

    made buttons word

--- static/index.js
+++ static/index.js
@@ -3,10 +3,11 @@ let WIDTH = 800;
 
 var body = d3.select("body");
 var svg = body.append("svg")
-            .attr("width", WIDTH)
-            .attr("height", HEIGHT)
-            .attr("class", "bubble")
-            .style("border", "1px solid black");
+  .attr("width", WIDTH)
+  .attr("height", HEIGHT)
+  .attr("class", "bubble")
+  .style("border", "1px solid black")
+  .attr("id", "svgboi");
 
 // [category, project count, funding, backers]
 
@@ -25,14 +26,17 @@ var svg = body.append("svg")
 
 var hue = 0;
 var hue_step;
-var top_display="freq";
+var top_display = "freq";
 
 var changeDisplay = function (new_state) {
-  top_display=new_state;
-  makeChart(categories,top_display);
+  document.getElementById("svgboi").innerHTML = "";
+  console.log("clicked");
+  top_display = new_state;
+  go();
 }
 
-d3.csv(data_sr).then(function(data) {
+var go = function () {
+  d3.csv(data_sr).then(function (data) {
     // console.log(data);
 
     var cat_holder = [];
@@ -43,107 +47,117 @@ d3.csv(data_sr).then(function(data) {
 
     var categories = [];
 
-    data.forEach(function(d) {
-        // total num of projects
-        if (cat_holder.indexOf(d.main_category) == -1){
-            cat_holder.push(d.main_category);
-            cat_freq.push(1);
-            //console.log(d.backers);
-            cat_backers.push(parseInt(d.backers));
-            cat_fund.push(parseFloat(d.usd_pledged_real));
-        }
-        else {
-            cat_freq[cat_holder.indexOf(d.main_category)] += 1;
-            //console.log(d.backers);
-            cat_backers[cat_holder.indexOf(d.main_category)] += parseInt(d.backers);
-            cat_fund[cat_holder.indexOf(d.main_category)]+=parseFloat(d.usd_pledged_real);
-            //console.log("hat"+cat_backers[cat_holder.indexOf(d.main_category)]);
-        }
+    data.forEach(function (d) {
+      // total num of projects
+      if (cat_holder.indexOf(d.main_category) == -1) {
+        cat_holder.push(d.main_category);
+        cat_freq.push(1);
+        //console.log(d.backers);
+        cat_backers.push(parseInt(d.backers));
+        cat_fund.push(parseFloat(d.usd_pledged_real));
+      } else {
+        cat_freq[cat_holder.indexOf(d.main_category)] += 1;
+        //console.log(d.backers);
+        cat_backers[cat_holder.indexOf(d.main_category)] += parseInt(d.backers);
+        cat_fund[cat_holder.indexOf(d.main_category)] += parseFloat(d.usd_pledged_real);
+        //console.log("hat"+cat_backers[cat_holder.indexOf(d.main_category)]);
+      }
 
-        // Funding
+      // Funding
 
-        // number of Backers
+      // number of Backers
 
-        // success rate
+      // success rate
     });
 
-    cat_holder.forEach(function(d, i) {
-        var new_obj = {
-            name: d,
-            freq: cat_freq[i],
-            fund: cat_fund[i],
-            backers: cat_backers[i],
-            success: 0
-        };
-        categories.push(new_obj);
+    cat_holder.forEach(function (d, i) {
+      var new_obj = {
+        name: d,
+        freq: cat_freq[i],
+        fund: cat_fund[i],
+        backers: cat_backers[i],
+        success: 0
+      };
+      categories.push(new_obj);
     })
 
     hue_step = 360.0 / (categories.length + 1);
 
-    makeChart(categories,"freq");
+    makeChart(categories, top_display);
 
     // console.log(cat_holder);
     // console.log(cat_freq);
     // console.log(categories);
-});
+  });
+}
 
-var makeChart = function(data,thing) {
+var makeChart = function (data, thing) {
 
-    data = {"children": data};
-    console.log(data);
+  data = {
+    "children": data
+  };
+  console.log(data);
 
-    var bubble = d3.pack(data)
-                    .size([WIDTH-2, HEIGHT-2])
-                    .padding(1.5);;
+  var bubble = d3.pack(data)
+    .size([WIDTH - 2, HEIGHT - 2])
+    .padding(1.5);;
 
-    // console.log(bubble);
+  // console.log(bubble);
 
-    var nodes = d3.hierarchy(data)
-            .sum(function(d) { return d[thing]; });
+  var nodes = d3.hierarchy(data)
+    .sum(function (d) {
+      return d[thing];
+    });
 
-    var node = svg.selectAll(".node")
-            .data(bubble(nodes).descendants())
-            .enter()
-            .filter(function(d){
-                return  !d.children
-            })
-            .append("g")
-            .attr("class", "node")
-            .attr("transform", function(d) {
-                return "translate(" + d.x + "," + d.y + ")";
-            });
+  var node = svg.selectAll(".node")
+    .data(bubble(nodes).descendants())
+    .enter()
+    .filter(function (d) {
+      return !d.children
+    })
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", function (d) {
+      return "translate(" + d.x + "," + d.y + ")";
+    });
 
-    // console.log(node);
-    node.append("title")
-            .text(function(d) {
-                // console.log(d);
-                return d.data.name + ": " + d.data.freq;
-            });
+  // console.log(node);
+  node.append("title")
+    .text(function (d) {
+      // console.log(d);
+      return d.data.name + ": " + d.data.freq;
+    });
 
-    node.append("circle")
-        .attr("r", function(d) {
-                return d.r;
-        })
-        .style("fill", function() {
-          var c = d3.color("hsl(" + hue + ", 90%, 50%)");
-          hue += hue_step;
-          return c;
-        });
+  node.append("circle")
+    .attr("r", function (d) {
+      return d.r;
+    })
+    .style("fill", function () {
+      var c = d3.color("hsl(" + hue + ", 90%, 50%)");
+      hue += hue_step;
+      return c;
+    });
 
-    node.append("text")
-            .attr("dy", ".3em")
-            .style("text-anchor", "middle")
-            .attr("class", "cir_text")
-            .attr("textLength", function(d) {return 1.7*d.r})
-            .text(function(d) {
-                return d.data.name;
-            });
+  node.append("text")
+    .attr("dy", ".3em")
+    .style("text-anchor", "middle")
+    .style('fill', 'black')
+    .attr("class", "cir_text")
+    .text(function (d) {
+      return d.data.name;
+    });
+  // this makes the text wEird
+  //    .attr("textLength", function (d) {
+  //      return 1.7 * d.r
+  //    });
 };
 
 
-    // var max_projects = 0;
-    // var max_funding = 0;
-    // var max_backers = 0;
+go();
+
+// var max_projects = 0;
+// var max_funding = 0;
+// var max_backers = 0;
 //
 // for (let item of data) {
 //   if (item[1] > max_projects) max_projects = item[1];
